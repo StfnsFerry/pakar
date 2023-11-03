b577665d60842e9a63ba13190568debe508dc6d0
Hafizh0192 <alhafizh0192@gmail.com>
2023-11-02 13:53:24 -1200

    add Dashboard Penjual

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,6 +2,7 @@
 
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\AdminController;
+use App\Controllers\PenjualController;
 
 /**
  * @var RouteCollection $routes
@@ -12,3 +13,7 @@ $routes->get('/admin', [AdminController::class, 'index']);
 $routes->get('/admin/toko', [AdminController::class, 'toko']);
 $routes->get('/admin/transaksi', [AdminController::class, 'transaksi']);
 $routes->get('/admin/pengembalian', [AdminController::class, 'pengembalian']);
+
+$routes->get('/penjual', [PenjualController::class, 'index']);
+$routes->get('/penjual/pesanan', [PenjualController::class, 'pesanan']);
+$routes->get('/penjual/transaksi', [PenjualController::class, 'transaksi']);
